ab26cfa91c0ae39af7a0f0e7a06acc9880211d8e
Maciej Banaś <74212933+maciekbanas@users.noreply.github.com>
2026-05-22 22:37:30 +0200

    Add missing method documentation across R6 classes

--- R/images.R
+++ R/images.R
@@ -1,6 +1,14 @@
 Image <- R6::R6Class(
   classname = "Image",
   public = list(
+    #' @description Add an image object to the Phaser scene.
+    #' @param name Character. Unique name of the image.
+    #' @param url Character. URL or path to image file.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
+    #' @param visible Logical. Whether image is initially visible.
+    #' @param clickable Logical. Whether image emits click events.
+    #' @param session Shiny session object.
     initialize = function(name, url, x, y, visible, clickable,
                           session = getDefaultReactiveDomain()) {
       private$session <- session
@@ -9,10 +17,12 @@ Image <- R6::R6Class(
                     name, url, x, y, tolower(visible), tolower(clickable))
       send_js(private, js)
     },
+    #' @description Show a previously added image.
     show = function() {
       js <- sprintf("showImage('%s');", private$name)
       send_js(private, js)
     },
+    #' @description Hide a previously added image.
     hide = function() {
       js <- sprintf("hideImage('%s');", private$name)
       send_js(private, js)
--- R/rectangle.R
+++ R/rectangle.R
@@ -1,6 +1,16 @@
 Rectangle <- R6::R6Class(
   classname = "Rectangle",
   public = list(
+    #' @description Add a rectangle object to the Phaser scene.
+    #' @param name Character. Unique name of the rectangle.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
+    #' @param width Numeric. Rectangle width in pixels.
+    #' @param height Numeric. Rectangle height in pixels.
+    #' @param color Character. Fill color in Phaser-compatible format.
+    #' @param visible Logical. Whether rectangle is initially visible.
+    #' @param clickable Logical. Whether rectangle emits click events.
+    #' @param session Shiny session object.
     initialize = function(name, x, y, width, height, color, visible, clickable,
                           session = getDefaultReactiveDomain()) {
       private$session <- session
@@ -9,10 +19,12 @@ Rectangle <- R6::R6Class(
                     name, x, y, width, height, color, tolower(visible), tolower(clickable))
       send_js(private, js)
     },
+    #' @description Show a previously added rectangle.
     show = function() {
       js <- sprintf("showImage('%s');", private$name)
       send_js(private, js)
     },
+    #' @description Hide a previously added rectangle.
     hide = function() {
       js <- sprintf("hideImage('%s');", private$name)
       send_js(private, js)
